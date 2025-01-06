06d8788602e048d815cd71a28b99cd580451cb56
Mukunthan <mukunthan.1255@gamil.com>
2025-01-06 22:06:55 +0530

    Changed color code.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,13 +13,6 @@ const Card = ({ id, title, children, status = "pending" }) => {
 
   const currentTask = taskData.find((task) => task.id === id);
 
-  const bgColor =
-    selectedOption === "active"
-      ? "bg-green-200"
-      : selectedOption === "completed"
-      ? "bg-green-700"
-      : "bg-red-300";
-
   const handleSetActive = () => {
     setSelectedOption("active");
     setDropdownVisible(false);
@@ -70,43 +63,52 @@ const Card = ({ id, title, children, status = "pending" }) => {
     <div className="flex justify-center mt-4">
       {/* Card with Heading and Description */}
       <div
-        className={`w-full max-w-md py-5 px-5  rounded-lg shadow-lg relative ${bgColor} `}
+        className={`w-full max-w-md py-5 px-5  rounded-lg shadow-lg relative bg-gray-100 `}
       >
         <h2 className="text-xl font-bold mb-1">{title}</h2>
         <p className="text-base mb-2">{children}</p>
 
-        <div className="mt-6">
-          {selectedOption && (
-            <p className="bg-gray-100 p-3 rounded-md text-sm">
-              Selected Option: {selectedOption}
-            </p>
-          )}
-        </div>
-
-        {/* Three dot button on the top-right */}
+        {/* Three dot or cross button on the top-right */}
         <button
           onClick={toggleDropdown}
           className="absolute top-4 right-4 focus:outline-none"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 12h12M6 6h12M6 18h12"
-            />
-          </svg>
+          {dropdownVisible ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 12h12M6 6h12M6 18h12"
+              />
+            </svg>
+          )}
         </button>
 
         {/* Dropdown Menu */}
         {dropdownVisible && (
-          <div className="absolute top-12 right-4 bg-white text-black rounded-lg shadow-lg w-40">
+          <div className="absolute top-12 right-4 bg-white text-black rounded-lg shadow-lg w-40 z-10">
             <ul className="px-2 py-1">
               <li
                 className={`cursor-pointer hover:bg-gray-200 px-2 py-1 rounded ${
--- src/components/Pending.jsx
+++ src/components/Pending.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 const Pending = ({ tasks }) => {
   const pendingTasks = tasks.filter((task) => task.status === "pending");
   return (
-    <div className="flex-col bg-gray-100 p-4 rounded-lg shadow m-2">
+    <div className="flex-col bg-red-400 p-4 rounded-lg shadow m-2">
       <h2 className="text-gray-800 font-bold text-center">PENDING</h2>
       <p className="text-gray-600">
         This is the first container with light grey background.
