95c08c418b51aab30d3e8fd7abbd1157714db559
Mukunthan <mukunthan.1255@gamil.com>
2025-01-07 22:15:37 +0530

    Adding stats charts

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,48 +2,18 @@
 import React, { useState, useEffect } from "react";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from "chart.js";
-import { useRecoilState } from "recoil";
-import currentTasks from "../data/atoms";
 
 // Register necessary Chart.js components
 ChartJS.register(ArcElement, Tooltip, Legend, Title);
 
-const Dashboard = () => {
-  const [taskData, setTaskData] = useRecoilState(currentTasks);
-
-  // State for task counts
-  const [taskCount, setTaskCount] = useState({
-    pending: 0,
-    active: 0,
-    completed: 0,
-  });
-
-  // UseEffect to calculate task counts when taskData changes
-  useEffect(() => {
-    const newTaskCount = { pending: 0, active: 0, completed: 0 };
-
-    // Iterate over taskData and update the count
-    taskData.forEach((task) => {
-      if (task.status === "pending") {
-        newTaskCount.pending += 1;
-      } else if (task.status === "active") {
-        newTaskCount.active += 1;
-      } else if (task.status === "completed") {
-        newTaskCount.completed += 1;
-      }
-    });
-
-    // Update task count state after calculations
-    setTaskCount(newTaskCount);
-  }, [taskData]); // Re-run when taskData changes
-
+const Dashboard = ({ children }) => {
   // Data for the pie chart
   const data = {
     labels: ["Pending", "Completed", "Active"],
     datasets: [
       {
         label: "Task Distribution",
-        data: [taskCount.pending, taskCount.completed, taskCount.active], // Use state values
+        data: [children.pending, children.completed, children.active], // Use state values
         backgroundColor: [
           "rgba(255, 99, 132, 0.2)",
           "rgba(40, 176, 77, 0.2)",
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,17 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoStatsChartOutline } from "react-icons/io5";
 import Dashboard from "./Dashboard";
+import { useRecoilState } from "recoil";
+import currentTasks from "../data/atoms";
 
 const EmptyDialog = ({ isOpen, onClose }) => {
+  const [taskData, setTaskData] = useRecoilState(currentTasks);
+
+  // State for task counts
+  const [taskCount, setTaskCount] = useState({
+    pending: 0,
+    active: 0,
+    completed: 0,
+  });
+
+  // UseEffect to calculate task counts when taskData changes
+  useEffect(() => {
+    const newTaskCount = { pending: 0, active: 0, completed: 0 };
+
+    // Iterate over taskData and update the count
+    taskData.forEach((task) => {
+      if (task.status === "pending") {
+        newTaskCount.pending += 1;
+      } else if (task.status === "active") {
+        newTaskCount.active += 1;
+      } else if (task.status === "completed") {
+        newTaskCount.completed += 1;
+      }
+    });
+
+    // Update task count state after calculations
+    setTaskCount(newTaskCount);
+  }, [taskData]); // Re-run when taskData changes
+
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-xl max-w-sm w-full">
-        {/* Empty dialog content goes here */}
-        <Dashboard></Dashboard>
-        <div className="flex justify-end space-x-4">
-          {/* Close button */}
+      <div className="bg-white p-6 rounded-lg shadow-xl w-auto w-full">
+        <div className="flex ">
+          <div>
+            <h3 className="font-bold">Stats</h3>
+          </div>
+          <Dashboard>{taskCount}</Dashboard>
+        </div>
+        <div className="flex justify-end space-x-4 pt-2">
           <button
             onClick={onClose}
             className="px-4 py-2 bg-gray-300 text-gray-800 rounded-md hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-500"
